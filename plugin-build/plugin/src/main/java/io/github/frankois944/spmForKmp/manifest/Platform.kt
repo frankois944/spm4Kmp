81d08b76a04289d6db3b616dca43c54bfdb4d398
frankois <dabonot.francois@gmail.com>
2025-01-06 13:53:07 +0100

    use constant for defining min platform version

---
+++ plugin-build/plugin/src/main/java/io/github/frankois944/spmForKmp/manifest/Platform.kt
@@ -0,0 +1,26 @@
+package io.github.frankois944.spmForKmp.manifest
+
+/**
+ * DEFAULT_MIN_IOS_VERSION: 12.0
+ */
+internal const val DEFAULT_MIN_IOS_VERSION = "12.0"
+
+/**
+ * DEFAULT_MIN_TV_OS_VERSION: 12.0
+ */
+internal const val DEFAULT_MIN_TV_OS_VERSION = "12.0"
+
+/**
+ * DEFAULT_MIN_WATCH_OS_VERSION: 4.0
+ */
+internal const val DEFAULT_MIN_WATCH_OS_VERSION = "4.0"
+
+/**
+ * DEFAULT_MIN_MAC_OS_VERSION: 10.3
+ */
+internal const val DEFAULT_MIN_MAC_OS_VERSION = "10.13"
+
+/**
+ * DEFAULT_TOOL_VERSION: 5.9
+ */
+internal const val DEFAULT_TOOL_VERSION = "5.9"
