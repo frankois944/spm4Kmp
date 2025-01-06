7689610c8461f0cce2505afd82a56e4426b75eee
frankois <dabonot.francois@gmail.com>
2025-01-06 19:31:09 +0100

    new tests

---
+++ plugin-build/plugin/src/functionalTest/kotlin/io/github/frankois944/spmForKmp/ImplicitDepPackageTest.kt
@@ -0,0 +1,57 @@
+package io.github.frankois944.spmForKmp
+
+import com.autonomousapps.kit.GradleBuilder
+import com.autonomousapps.kit.truth.TestKitTruth.Companion.assertThat
+import io.github.frankois944.spmForKmp.definition.SwiftDependency
+import io.github.frankois944.spmForKmp.fixture.KotlinSource
+import io.github.frankois944.spmForKmp.fixture.SmpKMPTestFixture
+import io.github.frankois944.spmForKmp.fixture.SwiftSource
+import io.github.frankois944.spmForKmp.utils.BaseTest
+import org.junit.jupiter.api.Test
+
+class ImplicitDepPackageTest : BaseTest() {
+    @Test
+    fun `build with implicit package dependency`() {
+        // Given
+        val fixture =
+            SmpKMPTestFixture
+                .builder()
+                .withBuildPath(testProjectDir.root.absolutePath)
+                .withDependencies(
+                    buildList {
+                        add(
+                            SwiftDependency.Package.Remote.Version(
+                                url = "https://github.com/google/GoogleSignIn-iOS",
+                                names = listOf("GoogleSignIn"),
+                                packageName = "GoogleSignIn-iOS",
+                                version = "8.0.0",
+                                exportToKotlin = true,
+                            ),
+                        )
+                    },
+                ).withKotlinSources(
+                    KotlinSource.of(
+                        content =
+                            """
+                            package com.example
+                            import GoogleSignIn.GIDSignIn
+                            """.trimIndent(),
+                    ),
+                ).withSwiftSources(
+                    SwiftSource.of(
+                        content =
+                            """
+                            import GoogleSignIn
+                            """.trimIndent(),
+                    ),
+                ).build()
+
+        val result =
+            GradleBuilder
+                .runner(fixture.gradleProject.rootDir, "build")
+                .build()
+
+        // Then
+        assertThat(result).task(":library:build").succeeded()
+    }
+}
