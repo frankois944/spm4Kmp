b33101e31cc85c284d3e69e2a616cddb7b01a61a
frankois <dabonot.francois@gmail.com>
2025-01-14 13:58:21 +0100

    add missing stuff

---
+++ example/exportedNativeShared/Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+  name: "exportedNativeShared",
+  platforms: [.iOS("12.0"), .macOS("10.15"), .tvOS("12.0"), .watchOS("4.0")],
+  products: [
+    .library(
+      name: "exportedNativeShared",
+      type: .static,
+      targets: ["exportedNativeShared", "DummyFramework"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "11.6.0"),
+    .package(
+      path:
+        "/Users/francoisdabonot/DEV/spm-kmp-plugin/example/../plugin-build/plugin/src/functionalTest/resources/LocalSourceDummyFramework"
+    ),
+  ],
+  targets: [
+    .target(
+      name: "exportedNativeShared",
+      dependencies: [
+        .product(name: "FirebaseCore", package: "firebase-ios-sdk"),
+        .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"), "DummyFramework",
+        .product(name: "LocalSourceDummyFramework", package: "LocalSourceDummyFramework"),
+      ],
+      path: "Sources"),
+    .binaryTarget(
+      name: "DummyFramework",
+      path: "../../plugin-build/plugin/src/functionalTest/resources/DummyFramework.xcframework.zip"),
+  ]
+)
