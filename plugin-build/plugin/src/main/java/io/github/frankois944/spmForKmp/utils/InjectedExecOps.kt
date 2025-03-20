40b93a196b95fc26f5628d83907f1be2cff07294
frankois <dabonot.francois@gmail.com>
2025-03-20 06:13:59 +0100

    remove useless file

--- plugin-build/plugin/src/main/java/io/github/frankois944/spmForKmp/utils/InjectedExecOps.kt
+++
@@ -1,9 +0,0 @@
-package io.github.frankois944.spmForKmp.utils
-
-import org.gradle.process.ExecOperations
-import javax.inject.Inject
-
-internal interface InjectedExecOps {
-    @get:Inject
-    val execOps: ExecOperations
-}
