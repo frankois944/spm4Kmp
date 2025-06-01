be821fc1bc08af011d370f52f932a0ca4a63163c
frankois <dabonot.francois@gmail.com>
2025-06-01 23:29:06 +0200

    wip

--- plugin-build/plugin/src/main/java/io/github/frankois944/spmForKmp/resources/CopiedResourcesFactory.kt
+++ plugin-build/plugin/src/main/java/io/github/frankois944/spmForKmp/resources/CopiedResourcesFactory.kt
@@ -20,131 +20,115 @@ internal data class FrameworkResource(
 
 @Suppress("LongParameterList")
 internal class CopiedResourcesFactory(
-    private val packageScratchDir: File,
-    baseDir: File,
-    private val platformName: String,
-    private val archs: String,
-    private val buildPackageMode: String,
+    private val inputBuiltDirectory: File,
     contentFolderPath: String,
     buildProductDir: String,
     private val logger: Logger,
 ) {
     val outputBundleDirectory: File
     val outputFrameworkDirectory: File
-    val frameworks: List<FrameworkResource>
-    val bundles: List<File>
+    val frameworks: List<FrameworkResource> =
+        buildList {
+            inputBuiltDirectory
+                .listFiles {
+                    it.extension == "framework"
+                }.forEach { framework ->
+                    val plist = framework.resolve("Info.plist")
+                    logger.debug("Looking inside the Info.plist {}", plist)
+                    val libraryName = getPlistValue(plist, "CFBundleExecutable")
+                    logger.debug("Found libraryName $libraryName")
+                    val binaryFile = framework.resolve(libraryName)
+                    val newFramework = FrameworkResource(framework, binaryFile = binaryFile)
+                    add(newFramework)
+                }
+        }
+    val bundles: List<File> =
+        buildList {
+            // get all bundle folders from build directory
+            inputBuiltDirectory
+                .listFiles {
+                    it.extension == "bundle"
+                }.let {
+                    addAll(it)
+                }
+            // get all bundles from frameworks
+            inputBuiltDirectory
+                .listFiles {
+                    it.extension == "framework"
+                }.forEach { framework ->
+                    framework
+                        .listFiles {
+                            it.extension == "bundle"
+                        }?.let {
+                            addAll(it)
+                        }
+                }
+        }
 
     init {
-        val inputBuiltDirectory =
-            getCurrentPackagesBuiltDir(
-                packageScratchDir = packageScratchDir,
-                platformName = platformName,
-                archs = archs,
-                buildPackageMode = buildPackageMode,
-                logger = logger,
-            )
-
-        bundles =
-            buildList {
-                // get all bundle folders from build directory
-                inputBuiltDirectory
-                    .listFiles {
-                        it.extension == "bundle"
-                    }.let {
-                        addAll(it)
-                    }
-                // get all bundles from frameworks
-                inputBuiltDirectory
-                    .listFiles {
-                        it.extension == "framework"
-                    }.forEach { framework ->
-                        framework
-                            .listFiles {
-                                it.extension == "bundle"
-                            }?.let {
-                                addAll(it)
-                            }
-                    }
-            }
-
-        frameworks =
-            buildList {
-                inputBuiltDirectory
-                    .listFiles {
-                        it.extension == "framework"
-                    }.forEach { framework ->
-                        val plist = framework.resolve("Info.plist")
-                        logger.debug("Looking inside the Info.plist {}", plist)
-                        val libraryName = getPlistValue(plist, "CFBundleExecutable")
-                        logger.debug("Found libraryName $libraryName")
-                        val binaryFile = framework.resolve(libraryName)
-                        val newFramework = FrameworkResource(framework, binaryFile = binaryFile)
-                        add(newFramework)
-                    }
-            }
 
         val destinationDir = Path(buildProductDir, contentFolderPath).toFile()
 
-        outputBundleDirectory = destinationDir.relativeTo(baseDir)
-        outputFrameworkDirectory = destinationDir.resolve("Frameworks").relativeTo(baseDir)
+        outputBundleDirectory = destinationDir
+        outputFrameworkDirectory = destinationDir.resolve("Frameworks")
         logger.debug("outputBundleDirectory: {}", outputBundleDirectory)
         logger.debug("outputFrameworkDirectory: {}", outputFrameworkDirectory)
         logger.debug("inputBundles: {}", bundles.map { it.name })
         logger.debug("inputFrameworks: {}", frameworks.map { it.name + it.files.map { f -> f.name } })
     }
+}
 
-    private fun getCurrentPackagesBuiltDir(
-        packageScratchDir: File,
-        platformName: String,
-        archs: String,
-        buildPackageMode: String,
-        logger: Logger,
-    ): File {
-        logger.debug("Looking for a match with platformName $platformName")
-        val systemType: String? =
-            when {
-                platformName.contains("iphone") -> {
-                    "ios"
-                }
-
-                platformName.contains("watch") -> {
-                    "watchos"
-                }
-
-                platformName.contains("mac") -> {
-                    "macosx"
-                }
-
-                platformName.contains("tv") -> {
-                    "tvos"
-                }
-
-                else -> {
-                    null
-                }
+internal fun getCurrentPackagesBuiltDir(
+    packageScratchDir: File,
+    platformName: String,
+    archs: String,
+    buildPackageMode: String,
+    logger: Logger,
+): File {
+    logger.debug("Looking for a match with platformName $platformName")
+    val systemType: String? =
+        when {
+            platformName.contains("iphone") -> {
+                "ios"
             }
-        if (systemType == null) {
-            throw RuntimeException("No matching systemType from platformName $platformName")
-        }
-        val simulator: String =
-            if (platformName.contains("simulator")) {
-                "-simulator"
-            } else {
-                ""
+
+            platformName.contains("watch") -> {
+                "watchos"
             }
-        val buildPackageDirName = "$archs-apple-$systemType$simulator"
-        logger.debug("buildPackageDir created $buildPackageDirName")
-        val buildPackagePath =
-            packageScratchDir
-                .resolve(buildPackageDirName)
-                .resolve(buildPackageMode)
-                .toPath()
-        if (!buildPackagePath.exists()) {
-            logger.error("The buildPackagePath doesn't exist at $buildPackagePath")
-            throw RuntimeException("Can't find the package build dir")
-        } else {
-            logger.debug("Found {} as packages resources path", buildPackagePath)
+
+            platformName.contains("mac") -> {
+                "macosx"
+            }
+
+            platformName.contains("tv") -> {
+                "tvos"
+            }
+
+            else -> {
+                null
+            }
         }
-        return buildPackagePath.toFile()
+    if (systemType == null) {
+        throw RuntimeException("No matching systemType from platformName $platformName")
     }
+    val simulator: String =
+        if (platformName.contains("simulator")) {
+            "-simulator"
+        } else {
+            ""
+        }
+    val buildPackageDirName = "$archs-apple-$systemType$simulator"
+    logger.debug("buildPackageDir created $buildPackageDirName")
+    val buildPackagePath =
+        packageScratchDir
+            .resolve(buildPackageDirName)
+            .resolve(buildPackageMode)
+            .toPath()
+    if (!buildPackagePath.exists()) {
+        logger.error("The buildPackagePath doesn't exist at $buildPackagePath")
+        throw RuntimeException("Can't find the package build dir")
+    } else {
+        logger.debug("Found {} as packages resources path", buildPackagePath)
+    }
+    return buildPackagePath.toFile()
 }
