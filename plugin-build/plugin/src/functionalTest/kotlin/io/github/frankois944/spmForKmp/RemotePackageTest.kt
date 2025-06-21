e733be99892306162dfdf695344189a236683c07
frankois <dabonot.francois@gmail.com>
2025-06-21 21:59:34 +0200

    update tests

--- plugin-build/plugin/src/functionalTest/kotlin/io/github/frankois944/spmForKmp/RemotePackageTest.kt
+++ plugin-build/plugin/src/functionalTest/kotlin/io/github/frankois944/spmForKmp/RemotePackageTest.kt
@@ -166,7 +166,7 @@ remotePackageCommit(
                     KotlinSource.of(
                         content =
                             """
-remotePackageCommit(
+remotePackageVersion(
     url = URI("https://github.com/firebase/firebase-ios-sdk.git"),
     version = "11.6.0",
     packageName = "",
