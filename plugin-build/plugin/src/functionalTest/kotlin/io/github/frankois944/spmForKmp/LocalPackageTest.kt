556f9b1865ada09096c4b6c13506cd77a38cde05
frankois <dabonot.francois@gmail.com>
2025-07-05 11:53:25 +0200

    remove useless tests

--- plugin-build/plugin/src/functionalTest/kotlin/io/github/frankois944/spmForKmp/LocalPackageTest.kt
+++ plugin-build/plugin/src/functionalTest/kotlin/io/github/frankois944/spmForKmp/LocalPackageTest.kt
@@ -73,67 +73,6 @@ class LocalPackageTest : BaseTest() {
     }
 
     @Test
-    fun `build with raw config`() {
-        val localPackageDirectory = File("src/functionalTest/resources/LocalSourceDummyFramework")
-        // Given
-        val fixture =
-            SmpKMPTestFixture
-                .builder()
-                .withBuildPath(testProjectDir.root.absolutePath)
-                .also {
-                    if (isCI) {
-                        it.withTargets(
-                            AppleCompileTarget.macosArm64,
-                            AppleCompileTarget.watchosArm64,
-                            AppleCompileTarget.watchosSimulatorArm64,
-                            AppleCompileTarget.tvosArm64,
-                            AppleCompileTarget.tvosSimulatorArm64,
-                            AppleCompileTarget.iosArm64,
-                        )
-                    } else {
-                        it.withTargets(
-                            AppleCompileTarget.macosArm64,
-                        )
-                    }
-                }.withRawDependencies(
-                    KotlinSource.of(
-                        content =
-                            """
-                            localPackage(
-                                path = "${localPackageDirectory.absolutePath}",
-                                products = {
-                                    add("LocalSourceDummyFramework", exportToKotlin = true)
-                                },
-                            )
-                            """.trimIndent(),
-                    ),
-                ).withKotlinSources(
-                    KotlinSource.of(
-                        imports = listOf("LocalSourceDummyFramework.LocalSourceDummy"),
-                    ),
-                ).withSwiftSources(
-                    SwiftSource.of(
-                        content =
-                            """
-                            import Foundation
-                            import LocalSourceDummyFramework
-                            @objc public class MySwiftDummyClass: NSObject {
-                            }
-                            """.trimIndent(),
-                    ),
-                ).build()
-
-        // When
-        val result =
-            GradleBuilder
-                .runner(fixture.gradleProject.rootDir, "build")
-                .build()
-
-        // Then
-        assertThat(result).task(":library:build").succeeded()
-    }
-
-    @Test
     fun `build with local packages and no swift code`() {
         val localPackageDirectory = File("src/functionalTest/resources/LocalSourceDummyFramework")
         // Given
