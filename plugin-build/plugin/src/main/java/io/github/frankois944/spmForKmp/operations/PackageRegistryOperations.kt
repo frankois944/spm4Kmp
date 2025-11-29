4416d93f4cb29c0464482e18b06a32c60c17bcc3
frankois <dabonot.francois@gmail.com>
2025-11-29 09:27:25 +0100

    add new ExecOperations method
    - add packageRegistry Set URL
    - add packageRegistry Login URL

---
+++ plugin-build/plugin/src/main/java/io/github/frankois944/spmForKmp/operations/PackageRegistryOperations.kt
@@ -0,0 +1,116 @@
+package io.github.frankois944.spmForKmp.operations
+
+import org.gradle.api.logging.Logger
+import org.gradle.process.ExecOperations
+import java.io.ByteArrayOutputStream
+import java.io.File
+
+internal fun ExecOperations.packageRegistrySet(
+    workingDir: File,
+    url: String,
+    logger: Logger,
+    swiftBinPath: String?,
+) {
+    // swift package-registry set
+    // [--global]
+    // [--scope <scope>]
+    // [--allow-insecure-http]
+    // <url>
+    val args =
+        buildList {
+            if (swiftBinPath == null) {
+                add("--sdk")
+                add("macosx")
+                add("swift")
+            }
+            add("package-registry")
+            add("set")
+            add("--allow-insecure-http")
+            add(url)
+        }
+
+    val standardOutput = ByteArrayOutputStream()
+    val errorOutput = ByteArrayOutputStream()
+    exec {
+        it.executable = swiftBinPath ?: "xcrun"
+        it.args = args
+        it.workingDir = workingDir
+        it.standardOutput = standardOutput
+        it.errorOutput = errorOutput
+        it.isIgnoreExitValue = true
+    }.also {
+        logger.printExecLogs(
+            "packageRegistrySet",
+            args,
+            it.exitValue != 0,
+            standardOutput,
+            errorOutput,
+        )
+    }
+}
+
+internal fun ExecOperations.packageRegistryAuth(
+    workingDir: File,
+    url: String,
+    username: String?,
+    password: String?,
+    token: String?,
+    tokenFile: String?,
+    logger: Logger,
+    swiftBinPath: String?,
+) {
+    // swift package-registry login
+    // [<url>]
+    // [--username <username>]
+    // [--password <password>]
+    // [--token <token>]
+    // [--token-file <token-file>]
+    // [--no-confirm]
+    val args =
+        buildList {
+            if (swiftBinPath == null) {
+                add("--sdk")
+                add("macosx")
+                add("swift")
+            }
+            add("package-registry")
+            add("login")
+            add(url)
+            username?.let {
+                add("--username")
+                add(it)
+            }
+            password?.let {
+                add("--password")
+                add(it)
+            }
+            token?.let {
+                add("--token")
+                add(it)
+            }
+            tokenFile?.let {
+                add("--token-file")
+                add(it)
+            }
+            add("--no-confirm")
+        }
+
+    val standardOutput = ByteArrayOutputStream()
+    val errorOutput = ByteArrayOutputStream()
+    exec {
+        it.executable = swiftBinPath ?: "xcrun"
+        it.args = args
+        it.workingDir = workingDir
+        it.standardOutput = standardOutput
+        it.errorOutput = errorOutput
+        it.isIgnoreExitValue = true
+    }.also {
+        logger.printExecLogs(
+            "packageRegistryAuth",
+            args,
+            it.exitValue != 0,
+            standardOutput,
+            errorOutput,
+        )
+    }
+}
