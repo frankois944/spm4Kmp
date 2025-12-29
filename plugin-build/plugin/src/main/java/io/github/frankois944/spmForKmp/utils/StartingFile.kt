3bc44510fc789b20a28fae0c43e79d04e5c49e52
frankois <dabonot.francois@gmail.com>
2025-12-29 12:06:23 +0100

    update the starting file

--- plugin-build/plugin/src/main/java/io/github/frankois944/spmForKmp/utils/StartingFile.kt
+++ plugin-build/plugin/src/main/java/io/github/frankois944/spmForKmp/utils/StartingFile.kt
@@ -12,7 +12,9 @@ internal object StartingFile {
         Ensure that your class is public and has the @objcMembers / @objc annotation.
         This file has been created because the folder is empty.
         Ignore this file if you don't need it.
+        **/
 
+        /**
         @objcMembers public class StartHere: NSObject {
             public override init() {
                 super.init()
