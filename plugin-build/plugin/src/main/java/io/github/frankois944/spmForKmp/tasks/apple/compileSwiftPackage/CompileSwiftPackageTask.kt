c1f30bb79246ce136507fae4ef3cc66283bd255c
frankois <dabonot.francois@gmail.com>
2026-03-18 16:53:00 +0100

    add toolchain property
    - we can now use a custom toolchain to build the package

--- plugin-build/plugin/src/main/java/io/github/frankois944/spmForKmp/tasks/apple/compileSwiftPackage/CompileSwiftPackageTask.kt
+++ plugin-build/plugin/src/main/java/io/github/frankois944/spmForKmp/tasks/apple/compileSwiftPackage/CompileSwiftPackageTask.kt
@@ -86,6 +86,10 @@ internal abstract class CompileSwiftPackageTask : DefaultTask() {
     @get:Internal
     abstract val storedTraceFile: RegularFileProperty
 
+    @get:Input
+    @get:Optional
+    abstract val toolchain: Property<String>
+
     @get:Inject
     abstract val execOps: ExecOperations
 
@@ -117,6 +121,10 @@ internal abstract class CompileSwiftPackageTask : DefaultTask() {
             val args =
                 buildList {
                     if (swiftBinPath.orNull == null) {
+                        toolchain.orNull?.let {
+                            add("--toolchain")
+                            add(it)
+                        }
                         add("--sdk")
                         add("macosx")
                         add("swift")
@@ -153,6 +161,7 @@ internal abstract class CompileSwiftPackageTask : DefaultTask() {
                     add("none")
                 }
 
+            println(args.joinToString(" "))
             val standardOutput = ByteArrayOutputStream()
             val errorOutput = ByteArrayOutputStream()
             tracer.trace("build") {
@@ -164,6 +173,9 @@ internal abstract class CompileSwiftPackageTask : DefaultTask() {
                         it.standardOutput = standardOutput
                         it.errorOutput = errorOutput
                         it.isIgnoreExitValue = true
+                        toolchain.orNull?.let { toolchain ->
+                            it.environment("TOOLCHAINS", toolchain)
+                        }
                     }.also {
                         logger.printExecLogs(
                             "buildPackage",
