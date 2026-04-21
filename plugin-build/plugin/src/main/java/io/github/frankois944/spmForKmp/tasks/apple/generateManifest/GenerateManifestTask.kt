47c2b8698ef9efa1dddcb999861efcca327938e6
frankois <dabonot.francois@gmail.com>
2026-04-21 08:28:06 +0200

    rollback import

--- plugin-build/plugin/src/main/java/io/github/frankois944/spmForKmp/tasks/apple/generateManifest/GenerateManifestTask.kt
+++ plugin-build/plugin/src/main/java/io/github/frankois944/spmForKmp/tasks/apple/generateManifest/GenerateManifestTask.kt
@@ -10,7 +10,12 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.Property
-import org.gradle.api.tasks.*
+import org.gradle.api.tasks.CacheableTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
 import org.gradle.process.ExecOperations
 import org.jetbrains.kotlin.konan.target.HostManager
 import javax.inject.Inject
